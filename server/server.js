efb99d8c93d9d220b24c0d5709f2e078acd3438b
tajkiatoma <tajkiatoma@gmail.com>
2020-12-06 18:20:25 +0600

    fix environment variable

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const puppeteer = require('puppeteer');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 async function scrapCnn() {
     try {
